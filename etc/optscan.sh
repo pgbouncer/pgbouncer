3ac6df0e53ac9c2c6c630c2baa0f6ebb34e8be08
Marko Kreen <markokr@gmail.com>
2016-02-14 13:56:28 +0200

    optscan: fix doc path

--- etc/optscan.sh
+++ etc/optscan.sh
@@ -3,7 +3,7 @@
 # Check if all options in main.c are defined in sample ini and docs
 
 sources="src/main.c"
-targets="doc/config.txt etc/pgbouncer.ini"
+targets="doc/config.rst etc/pgbouncer.ini"
 
 for opt in `grep CF_ABS "$sources" | sed -r 's/^[^"]*"([^"]*)".*/\1/'`; do
   for conf in $targets; do

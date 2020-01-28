efa76f2405a69f871da8afcd4ffacd0b5fc37dae
vthimmegowda <vthimmegowda@splunk.com>
2020-01-28 14:34:48 -0800

    update the schema name

--- src/objects.c
+++ src/objects.c
@@ -120,8 +120,8 @@ void init_objects(void)
 	aatree_init(&pam_user_tree, user_node_cmp, NULL);
 	user_cache = slab_create("user_cache", sizeof(PgUser), 0, NULL, USUAL_ALLOC);
 	db_cache = slab_create("db_cache", sizeof(PgDatabase), 0, NULL, USUAL_ALLOC);
+	schema_cache = slab_create("schema_cache", sizeof(PgSchema), 0, NULL, USUAL_ALLOC);
 	pool_cache = slab_create("pool_cache", sizeof(PgPool), 0, NULL, USUAL_ALLOC);
-	schema_cache = slab_create("db_cache", sizeof(PgSchema), 0, NULL, USUAL_ALLOC);
 
 	if (!user_cache || !db_cache || !pool_cache || !schema_cache)
 		fatal("cannot create initial caches");

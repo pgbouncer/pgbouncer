pgbouncer/pgbouncer

commit d0ada75158ef48c1a81369f0f403db4be6d84882

Set minimum pool size per database
This feature allows to set min_pool_size per database. The current
behavior uses the same min_pool_size setting for all databases. It could
not be suitable for some databases that has a different access pattern.

Let's say one database need a minimum number of connections to handle a
burst of connections in order to not increase the response time.
However, the other databases doesn't need such requirement. Hence, this
feature reduces the number of connections open by pgbouncer.

Closes #91

src/admin.c
@@ -492,9 +492,9 @@ static bool admin_show_databases(PgSocket *admin, const char *arg)
 		return true;
 	}
 
-	pktbuf_write_RowDescription(buf, "ssissiisiiii",
+	pktbuf_write_RowDescription(buf, "ssissiiisiiii",
 				    "name", "host", "port",
-				    "database", "force_user", "pool_size", "reserve_pool",
+				    "database", "force_user", "pool_size", "min_pool_size", "reserve_pool",
 				    "pool_mode", "max_connections", "current_connections", "paused", "disabled");
 	statlist_for_each(item, &database_list) {
 		db = container_of(item, PgDatabase, head);
@@ -504,10 +504,11 @@ static bool admin_show_databases(PgSocket *admin, const char *arg)
 		cv.value_p = &db->pool_mode;
 		if (db->pool_mode != POOL_INHERIT)
 			pool_mode_str = cf_get_lookup(&cv);
-		pktbuf_write_DataRow(buf, "ssissiisiiii",
+		pktbuf_write_DataRow(buf, "ssissiiisiiii",
 				     db->name, db->host, db->port,
 				     db->dbname, f_user,
 				     db->pool_size,
+					 db->min_pool_size,
 				     db->res_pool_size,
 				     pool_mode_str,
 				     database_max_connections(db),

src/janitor.c
@@ -447,7 +447,7 @@ static void check_unused_servers(PgPool *pool, struct StatList *slist, bool idle
 		} else if (server->state == SV_USED && !server->ready) {
 			disconnect_server(server, true, "SV_USED server got dirty");
 		} else if (cf_server_idle_timeout > 0 && idle > cf_server_idle_timeout
-			   && (cf_min_pool_size == 0 || pool_connected_server_count(pool) > cf_min_pool_size)) {
+			   && (pool->db->min_pool_size == 0 || pool_connected_server_count(pool) > pool->db->min_pool_size)) {
 			disconnect_server(server, true, "server idle timeout");
 		} else if (age >= cf_server_lifetime) {
 			if (pool->last_lifetime_disconnect + lifetime_kill_gap <= now) {
@@ -487,7 +487,7 @@ static void check_pool_size(PgPool *pool)
 	}
 
 	/* launch extra connections to satisfy min_pool_size */
-	if (cur < cf_min_pool_size &&
+	if (cur < pool->db->min_pool_size &&
 	    cur < pool->db->pool_size &&
 	    cf_pause_mode == P_NONE &&
 	    cf_reboot == 0 &&
@@ -744,6 +744,8 @@ void config_postprocess(void)
 		}
 		if (db->pool_size < 0)
 			db->pool_size = cf_default_pool_size;
+		if (db->min_pool_size < 0)
+			db->min_pool_size = cf_min_pool_size;
 		if (db->res_pool_size < 0)
 			db->res_pool_size = cf_res_pool_size;
 	}

src/objects.c
@@ -359,6 +359,8 @@ PgDatabase *register_auto_database(const char *name)
 		/* do not forget to check pool_size like in config_postprocess */
 		if (db->pool_size < 0)
 			db->pool_size = cf_default_pool_size;
+		if (db->min_pool_size < 0)
+			db->min_pool_size = cf_min_pool_size;
 		if (db->res_pool_size < 0)
 			db->res_pool_size = cf_res_pool_size;
 	}

include/bouncer.h
@@ -361,6 +361,7 @@ struct PgDatabase {
 	int port;
 
 	int pool_size;		/* max server connections in one pool */
+	int min_pool_size;	/* min server connections in one pool */
 	int res_pool_size;	/* additional server connections in case of trouble */
 	int pool_mode;		/* pool mode for this database */
 	int max_db_connections;	/* max server connections between all pools */

src/loader.c
@@ -180,6 +180,7 @@ bool parse_database(void *base, const char *name, const char *connstr)
 	PgDatabase *db;
 	struct CfValue cv;
 	int pool_size = -1;
+	int min_pool_size = -1;
 	int res_pool_size = -1;
 	int max_db_connections = -1;
 	int dbname_ofs;
@@ -248,6 +249,8 @@ bool parse_database(void *base, const char *name, const char *connstr)
 			timezone = val;
 		} else if (strcmp("pool_size", key) == 0) {
 			pool_size = atoi(val);
+		} else if (strcmp("min_pool_size", key) == 0) {
+			min_pool_size = atoi(val);
 		} else if (strcmp("reserve_pool", key) == 0) {
 			res_pool_size = atoi(val);
 		} else if (strcmp("max_db_connections", key) == 0) {
@@ -324,6 +327,7 @@ bool parse_database(void *base, const char *name, const char *connstr)
 
 	/* if pool_size < 0 it will be set later */
 	db->pool_size = pool_size;
+	db->min_pool_size = min_pool_size;
 	db->res_pool_size = res_pool_size;
 	db->pool_mode = pool_mode;
 	db->max_db_connections = max_db_connections;

commit e1081c7e57f0e3aa90b6a8f7a5d46dfa19c93c96

Fix reload of default_pool_size, min_pool_size, and res_pool_size
Reloading the global pool size parameters doesn't work because
config_postprocess() has no way to tell if the previous per-database
setting was explicitly set or copied from the global value.

Fix this by not doing the copying in config_postprocess() but instead
resolve whether to use the per-database or global setting when reading
the values.  Add some helper functions for this in server.c; similar
helper functions for other settings already existed there, so it's
more consistent now, too.

This also unbreaks test_min_pool_size broken by commit
d0ada75158ef48c1a81369f0f403db4be6d84882.  Add a similar test case for
res_pool_size, and also one for default_pool_size, for good measure.

fixes #248

include/server.h
@@ -19,5 +19,8 @@
 bool server_proto(SBuf *sbuf, SBufEvent evtype, struct MBuf *pkt)  _MUSTCHECK;
 void kill_pool_logins(PgPool *pool, const char *msg);
 int pool_pool_mode(PgPool *pool) _MUSTCHECK;
+int pool_pool_size(PgPool *pool) _MUSTCHECK;
+int pool_min_pool_size(PgPool *pool) _MUSTCHECK;
+int pool_res_pool_size(PgPool *pool) _MUSTCHECK;
 int database_max_connections(PgDatabase *db) _MUSTCHECK;
 int user_max_connections(PgUser *user) _MUSTCHECK;

src/admin.c
@@ -507,9 +507,9 @@ static bool admin_show_databases(PgSocket *admin, const char *arg)
 		pktbuf_write_DataRow(buf, "ssissiiisiiii",
 				     db->name, db->host, db->port,
 				     db->dbname, f_user,
-				     db->pool_size,
-				     db->min_pool_size,
-				     db->res_pool_size,
+				     db->pool_size >= 0 ? db->pool_size : cf_default_pool_size,
+				     db->min_pool_size >= 0 ? db->min_pool_size : cf_min_pool_size,
+				     db->res_pool_size >= 0 ? db->res_pool_size : cf_res_pool_size,
 				     pool_mode_str,
 				     database_max_connections(db),
 				     db->connection_count,

src/janitor.c
@@ -430,8 +430,8 @@ static void check_unused_servers(PgPool *pool, struct StatList *slist, bool idle
 	 * must be separated.  This avoids the need to re-launch lot
 	 * of connections together.
 	 */
-	if (pool->db->pool_size > 0)
-		lifetime_kill_gap = cf_server_lifetime / pool->db->pool_size;
+	if (pool_pool_size(pool) > 0)
+		lifetime_kill_gap = cf_server_lifetime / pool_pool_size(pool);
 
 	/* disconnect idle servers if needed */
 	statlist_for_each_safe(item, slist, tmp) {
@@ -447,7 +447,7 @@ static void check_unused_servers(PgPool *pool, struct StatList *slist, bool idle
 		} else if (server->state == SV_USED && !server->ready) {
 			disconnect_server(server, true, "SV_USED server got dirty");
 		} else if (cf_server_idle_timeout > 0 && idle > cf_server_idle_timeout
-			   && (pool->db->min_pool_size == 0 || pool_connected_server_count(pool) > pool->db->min_pool_size)) {
+			   && (pool_min_pool_size(pool) == 0 || pool_connected_server_count(pool) > pool_min_pool_size(pool))) {
 			disconnect_server(server, true, "server idle timeout");
 		} else if (age >= cf_server_lifetime) {
 			if (pool->last_lifetime_disconnect + lifetime_kill_gap <= now) {
@@ -471,9 +471,9 @@ static void check_pool_size(PgPool *pool)
 {
 	PgSocket *server;
 	int cur = pool_connected_server_count(pool);
-	int many = cur - (pool->db->pool_size + pool->db->res_pool_size);
+	int many = cur - (pool_pool_size(pool) + pool_res_pool_size(pool));
 
-	Assert(pool->db->pool_size >= 0);
+	Assert(pool_pool_size(pool) >= 0);
 
 	while (many > 0) {
 		server = first_socket(&pool->used_server_list);
@@ -487,8 +487,8 @@ static void check_pool_size(PgPool *pool)
 	}
 
 	/* launch extra connections to satisfy min_pool_size */
-	if (cur < pool->db->min_pool_size &&
-	    cur < pool->db->pool_size &&
+	if (cur < pool_min_pool_size(pool) &&
+	    cur < pool_pool_size(pool) &&
 	    cf_pause_mode == P_NONE &&
 	    cf_reboot == 0 &&
 	    pool_client_count(pool) > 0)
@@ -742,11 +742,5 @@ void config_postprocess(void)
 			kill_database(db);
 			continue;
 		}
-		if (db->pool_size < 0)
-			db->pool_size = cf_default_pool_size;
-		if (db->min_pool_size < 0)
-			db->min_pool_size = cf_min_pool_size;
-		if (db->res_pool_size < 0)
-			db->res_pool_size = cf_res_pool_size;
 	}
 }

src/main.c
@@ -418,7 +418,7 @@ void load_config(void)
 		}
 	}
 
-	/* reset pool_size, kill dbs */
+	/* kill dbs */
 	config_postprocess();
 
 	/* reopen logfile */
@@ -695,9 +695,9 @@ static void check_limits(void)
 	statlist_for_each(item, &database_list) {
 		db = container_of(item, PgDatabase, head);
 		if (db->forced_user)
-			fd_count += db->pool_size;
+			fd_count += (db->pool_size >= 0 ? db->pool_size : cf_default_pool_size);
 		else
-			fd_count += db->pool_size * total_users;
+			fd_count += (db->pool_size >= 0 ? db->pool_size : cf_default_pool_size) * total_users;
 	}
 
 	log_info("kernel file descriptor limit: %d (hard: %d); max_client_conn: %d, max expected fd use: %d",

src/objects.c
@@ -356,13 +356,6 @@ PgDatabase *register_auto_database(const char *name)
 	db = find_database(name);
 	if (db) {
 		db->db_auto = true;
-		/* do not forget to check pool_size like in config_postprocess */
-		if (db->pool_size < 0)
-			db->pool_size = cf_default_pool_size;
-		if (db->min_pool_size < 0)
-			db->min_pool_size = cf_min_pool_size;
-		if (db->res_pool_size < 0)
-			db->res_pool_size = cf_res_pool_size;
 	}
 
 	return db;
@@ -733,8 +726,8 @@ static bool life_over(PgSocket *server)
 	if (age < cf_server_lifetime)
 		return false;
 
-	if (pool->db->pool_size > 0)
-		lifetime_kill_gap = cf_server_lifetime / pool->db->pool_size;
+	if (pool_pool_size(pool) > 0)
+		lifetime_kill_gap = cf_server_lifetime / pool_pool_size(pool);
 
 	if (last_kill >= lifetime_kill_gap)
 		return true;
@@ -1182,26 +1175,26 @@ void launch_new_connection(PgPool *pool)
 	max = pool_server_count(pool);
 
 	/* when a cancel request is queued allow connections up to twice the pool size */
-	if (!statlist_empty(&pool->cancel_req_list) && max < (2 * pool->db->pool_size)) {
+	if (!statlist_empty(&pool->cancel_req_list) && max < (2 * pool_pool_size(pool))) {
 		log_debug("launch_new_connection: bypass pool limitations for cancel request");
 		goto force_new;
 	}
 
 	/* is it allowed to add servers? */
-	if (max >= pool->db->pool_size && pool->welcome_msg_ready) {
+	if (max >= pool_pool_size(pool) && pool->welcome_msg_ready) {
 		/* should we use reserve pool? */
-		if (cf_res_pool_timeout && pool->db->res_pool_size) {
+		if (cf_res_pool_timeout && pool_res_pool_size(pool)) {
 			usec_t now = get_cached_time();
 			PgSocket *c = first_socket(&pool->waiting_client_list);
 			if (c && (now - c->request_time) >= cf_res_pool_timeout) {
-				if (max < pool->db->pool_size + pool->db->res_pool_size) {
+				if (max < pool_pool_size(pool) + pool_res_pool_size(pool)) {
 					slog_warning(c, "taking connection from reserve_pool");
 					goto allow_new;
 				}
 			}
 		}
 		log_debug("launch_new_connection: pool full (%d >= %d)",
-				max, pool->db->pool_size);
+				max, pool_pool_size(pool));
 		return;
 	}
 

src/server.c
@@ -201,6 +201,30 @@ int pool_pool_mode(PgPool *pool)
 	return pool_mode;
 }
 
+int pool_pool_size(PgPool *pool)
+{
+	if (pool->db->pool_size < 0)
+		return cf_default_pool_size;
+	else
+		return pool->db->pool_size;
+}
+
+int pool_min_pool_size(PgPool *pool)
+{
+	if (pool->db->min_pool_size < 0)
+		return cf_min_pool_size;
+	else
+		return pool->db->min_pool_size;
+}
+
+int pool_res_pool_size(PgPool *pool)
+{
+	if (pool->db->res_pool_size < 0)
+		return cf_res_pool_size;
+	else
+		return pool->db->res_pool_size;
+}
+
 int database_max_connections(PgDatabase *db)
 {
 	if (db->max_db_connections <= 0) {

test/test.sh
@@ -559,6 +559,10 @@ test_pool_size() {
 	test `docount p0` -eq 2 || return 1
 	test `docount p1` -eq 5 || return 1
 
+	# test reload (GH issue #248)
+	admin "set default_pool_size = 7"
+	test `docount p1` -eq 7 || return 1
+
 	return 0
 }
 
@@ -589,6 +593,31 @@ test_min_pool_size() {
 	cnt=`psql -X -p $PG_PORT -tAq -c "select count(1) from pg_stat_activity where usename='bouncer' and datname='p1'" postgres`
 	echo $cnt
 	test "$cnt" -eq 3 || return 1
+}
+
+test_reserve_pool_size() {
+	# make existing connections go away
+	psql -X -p $PG_PORT -d postgres -c "select pg_terminate_backend(pid) from pg_stat_activity where usename='bouncer'"
+	until test $(psql -X -p $PG_PORT -d postgres -tAq -c "select count(1) from pg_stat_activity where usename='bouncer'") -eq 0; do sleep 0.1; done
+
+	# default_pool_size=5
+	admin "set reserve_pool_size = 3"
+
+	for i in {1..8}; do
+		psql -X -c "select pg_sleep(8)" p1 >/dev/null &
+	done
+	sleep 1
+	cnt=`psql -X -p $PG_PORT -tAq -c "select count(1) from pg_stat_activity where usename='bouncer' and datname='p1'" postgres`
+	echo $cnt
+	test "$cnt" -eq 5 || return 1
+
+	sleep 7  # reserve_pool_timeout + wiggle room
+
+	cnt=`psql -X -p $PG_PORT -tAq -c "select count(1) from pg_stat_activity where usename='bouncer' and datname='p1'" postgres`
+	echo $cnt
+	test "$cnt" -eq 8 || return 1
+
+	grep "taking connection from reserve_pool" $BOUNCER_LOG || return 1
 }
 
 test_max_db_connections() {
@@ -1274,6 +1303,7 @@ test_tcp_user_timeout
 test_max_client_conn
 test_pool_size
 test_min_pool_size
+test_reserve_pool_size
 test_max_db_connections
 test_max_user_connections
 test_online_restart
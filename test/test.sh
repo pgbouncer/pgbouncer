b825eeae9ae77194e6eab370346fb8ce32e57074
Goksel Simsek <20810681+goksgie@users.noreply.github.com>
2022-12-05 12:27:12 +0300

    [authdbname] Fix tests with correct user validation
    Previously, the tests were not validating with which user we
    were logging in as. This commit introduces those validations
    to complete the tests.

--- test/test.sh
+++ test/test.sh
@@ -961,13 +961,13 @@ test_auth_dbname() {
 
 	curuser=`psql -X -d "dbname=authdb user=someuser password=anypasswd" -tAq -c "select current_user;"`
 	echo "revert test curuser=$curuser"
-	test "$curuser" = "$curuser" || return 1
+	test "$curuser" = "someuser" || return 1
 	grep -q "Reverting to client database" $BOUNCER_LOG  || return 1
 
 	# test if auth_dbname specified in connection string takes precedence over global setting
 	curuser=`psql -X -d "dbname=pauthz user=someuser password=anypasswd" -tAq -c "select current_user;"`
 	echo "conn string test curuser=$curuser"
-	test "$curuser" = "$curuser" || return 1
+	test "$curuser" = "someuser" || return 1
 	grep -q "Authentication database authdb will be used instead of client's database pauthz for authentication." $BOUNCER_LOG  || return 1
 
 	# test if we reject on disabled database.
@@ -992,7 +992,7 @@ test_auth_dbname() {
 
 	curuser=`psql -X -d "dbname=postgres user=someuser password=anypasswd" -tAq -c "select current_user;"`
 	echo "default db test curuser=$curuser"
-	test "$curuser" = "$curuser" || return 1
+	test "$curuser" = "someuser" || return 1
 
 	echo "Grep result of default authentication test: $result"
 	grep -q "Authentication database authdb will be used instead of client's database postgres for authentication." $BOUNCER_LOG  || return 1
@@ -1005,7 +1005,7 @@ test_auth_dbname() {
 	admin "set auth_dbname=''"
 	curuser=`psql -X -d "dbname=p4 user=someuser password=anypasswd" -tAq -c "select current_user;"`
 	echo "curuser=$curuser"
-	test "$curuser" = "$curuser" || return 1
+	test "$curuser" = "puser1" || return 1
 
 	return 0
 }

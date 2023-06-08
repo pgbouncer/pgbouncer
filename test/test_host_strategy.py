53f82aaa7086b9b82ecc4696cee459aeb12c4f2c
Dave Pirotte <dpirotte@paypal.com>
2023-06-08 12:18:02 +0000

    Use unresolvable DNS rather than bad IP to test host_strategy
    Previously, the host_strategy tests used 127.0.0.2,127.0.0.1 had a "bad
    IP" (127.0.0.2) as the first entry to force the first backend server
    connection to fail and verify that `host_strategy=last_successful`
    directs new connections to the second "good" entry.
    
    MacOS doesn't treat 127.0.0.2 in the same manner as Linux by default, so
    we need a different way to fail the first host in the list. Switching to
    an unresolvable DNS hostname works fine on both Linux and MacOS.

--- test/test_host_strategy.py
+++ test/test_host_strategy.py
@@ -11,7 +11,7 @@ def test_host_strategy_last_successful_good_first(bouncer):
 def test_host_strategy_last_successful_bad_first(bouncer):
     # The first connection to a bogus host will fail
     bouncer.admin(f"set server_login_retry=1")
-    with bouncer.log_contains(r"127.0.0.3:\d+ closing because: connect failed", 1):
+    with bouncer.log_contains(r"closing because: server DNS lookup failed", 1):
         # The second connection to a good host will succeed and be reused by the subsequent connection
         with bouncer.log_contains(r"127.0.0.1:\d+ new connection to server", 1):
             bouncer.test(dbname="hostlist_bad_first")

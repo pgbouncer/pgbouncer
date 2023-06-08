46f6d0b59fc2cc607556a6ff585a69fb2df2e1b8
Dave Pirotte <dpirotte@paypal.com>
2023-06-08 15:03:01 +0000

    Fix host_strategy tests
    I incorrectly ported these tests from bash to pytest and so they were
    not actually verifying that subsequent connections were reusing the last
    successful host in the list. The tests now verify behavior correctly.

--- test/test_host_strategy.py
+++ test/test_host_strategy.py
@@ -2,18 +2,20 @@ import psycopg
 import pytest
 
 
-def test_host_strategy_last_successful_good_first(bouncer):
-    with bouncer.log_contains(r"127.0.0.1:\d+ new connection to server", 1):
-        bouncer.test(dbname="hostlist_good_first")
-        bouncer.test(dbname="hostlist_good_first")
+@pytest.mark.asyncio
+async def test_host_strategy_last_successful_good_first(bouncer):
+    with bouncer.log_contains(r"127.0.0.1:\d+ new connection to server", 2):
+        await bouncer.asleep(dbname="hostlist_good_first", duration=0.5, times=2)
 
 
-def test_host_strategy_last_successful_bad_first(bouncer):
-    # The first connection to a bogus host will fail
+@pytest.mark.asyncio
+async def test_host_strategy_last_successful_bad_first(bouncer):
     bouncer.admin(f"set server_login_retry=1")
     with bouncer.log_contains(r"closing because: server DNS lookup failed", 1):
-        # The second connection to a good host will succeed and be reused by the subsequent connection
-        with bouncer.log_contains(r"127.0.0.1:\d+ new connection to server", 1):
-            bouncer.test(dbname="hostlist_bad_first")
-            bouncer.test(dbname="hostlist_bad_first")
-            bouncer.test(dbname="hostlist_bad_first")
+        with bouncer.log_contains(r"127.0.0.1:\d+ new connection to server", 2):
+            # Execute two concurrent sleeps to force two backend connections.
+            # The first connection will attempt the "bad" host and retry on
+            # the "good" host.
+            # The second connection will honor `host_strategy` and use the
+            # `last_successful` host.
+            await bouncer.asleep(dbname="hostlist_bad_first", duration=0.5, times=2)

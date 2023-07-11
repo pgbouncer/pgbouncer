11336fd9e52c3c8c8577b0bc769a60415ce3f3c1
EmelSimsek <emelhasdal@gmail.com>
2023-07-11 12:36:06 +0300

    Comment, copyright, a new header file

--- include/varcache.h
+++ include/varcache.h
@@ -1,50 +1,5 @@
 #include <usual/strpool.h>
 
-#undef HASH_FUNCTION
-#define HASH_FUNCTION(keyptr,keylen,hashv) HASH_JEN_LOWERCASE(keyptr, keylen, hashv)
-
-#define HASH_JEN_LOWERCASE(key,keylen,hashv) \
-do {                                                                             \
-  unsigned _hj_i,_hj_j,_hj_k;                                                    \
-  unsigned const char *_hj_key=(unsigned const char*)(key);                      \
-  hashv = 0xfeedbeefu;                                                           \
-  _hj_i = _hj_j = 0x9e3779b9u;                                                   \
-  _hj_k = (unsigned)(keylen);                                                    \
-  while (_hj_k >= 12U) {                                                         \
-    _hj_i +=    (tolower(_hj_key[0]) + ( (unsigned)tolower(_hj_key[1]) << 8 )                      \
-	+ ( (unsigned)tolower(_hj_key[2]) << 16 )                                         \
-	+ ( (unsigned)tolower(_hj_key[3]) << 24 ) );                                      \
-    _hj_j +=    (tolower(_hj_key[4]) + ( (unsigned)tolower(_hj_key[5]) << 8 )                      \
-	+ ( (unsigned)tolower(_hj_key[6]) << 16 )                                         \
-	+ ( (unsigned)tolower(_hj_key[7]) << 24 ) );                                      \
-    hashv += (tolower(_hj_key[8]) + ( (unsigned)tolower(_hj_key[9]) << 8 )                         \
-	+ ( (unsigned)tolower(_hj_key[10]) << 16 )                                        \
-	+ ( (unsigned)tolower(_hj_key[11]) << 24 ) );                                     \
-     HASH_JEN_MIX(_hj_i, _hj_j, hashv);                                          \
-     _hj_key += 12;                                                              \
-     _hj_k -= 12U;                                                               \
-  }                                                                              \
-  hashv += (unsigned)(keylen);                                                   \
-  switch ( _hj_k ) {                                                             \
-    case 11: hashv += ( (unsigned)tolower(_hj_key[10]) << 24 ); /* FALLTHROUGH */         \
-    case 10: hashv += ( (unsigned)tolower(_hj_key[9]) << 16 );  /* FALLTHROUGH */         \
-    case 9:  hashv += ( (unsigned)tolower(_hj_key[8]) << 8 );   /* FALLTHROUGH */         \
-    case 8:  _hj_j += ( (unsigned)tolower(_hj_key[7]) << 24 );  /* FALLTHROUGH */         \
-    case 7:  _hj_j += ( (unsigned)tolower(_hj_key[6]) << 16 );  /* FALLTHROUGH */         \
-    case 6:  _hj_j += ( (unsigned)tolower(_hj_key[5]) << 8 );   /* FALLTHROUGH */         \
-    case 5:  _hj_j += tolower(_hj_key[4]);                      /* FALLTHROUGH */         \
-    case 4:  _hj_i += ( (unsigned)tolower(_hj_key[3]) << 24 );  /* FALLTHROUGH */         \
-    case 3:  _hj_i += ( (unsigned)tolower(_hj_key[2]) << 16 );  /* FALLTHROUGH */         \
-    case 2:  _hj_i += ( (unsigned)tolower(_hj_key[1]) << 8 );   /* FALLTHROUGH */         \
-    case 1:  _hj_i += tolower(_hj_key[0]);                      /* FALLTHROUGH */         \
-    default: ;                                                                   \
-  }                                                                              \
-  HASH_JEN_MIX(_hj_i, _hj_j, hashv);                                             \
-} while (0)
-
-#undef HASH_KEYCMP
-#define HASH_KEYCMP(a,b,len) (strcasecmp(a,b))
-
 enum VarCacheIdx {
 	VDateStyle = 0,
 	VClientEncoding,

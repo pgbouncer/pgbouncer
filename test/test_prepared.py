c8c987bff22b9846ad418042808d3419538f4177
Jelte Fennema-Nio <jelte.fennema@microsoft.com>
2023-11-01 10:46:06 +0100

    Modify tests a bit

--- test/test_prepared.py
+++ test/test_prepared.py
@@ -75,14 +75,40 @@ def test_deallocate_all(bouncer):
             # Run the prepared query again on server 2 and client 2
             cur2.execute(prepared_query)
 
+            # Confirm that the prepared query is not available anymore on
+            # client 1
+            with pytest.raises(
+                psycopg.OperationalError, match="prepared statement did not exist"
+            ):
+                cur1.execute(prepared_query)
+
+
+def test_discard_all(bouncer):
+    bouncer.admin(f"set pool_mode=transaction")
+    bouncer.admin(f"set max_prepared_statements=100")
+    prepared_query = "SELECT 1"
+    with bouncer.cur() as cur1:
+        with bouncer.cur() as cur2:
             # prepare query on client 1
-            cur1.execute(prepared_query2, prepare=True)
+            cur1.execute(prepared_query, prepare=True)
+            # Run the prepared query again on same server and client
+            cur1.execute(prepared_query)
+
+            # prepared query for client 2
+            cur2.execute(prepared_query, prepare=True)
 
             # execute DISCARD ALL on client 1
-            cur2.execute("DISCARD ALL")
+            cur1.execute("DISCARD ALL")
 
-            # Run the prepared query again on client 2
-            cur2.execute(prepared_query2)
+            # Run the prepared query again on server 2 and client 2
+            cur2.execute(prepared_query)
+
+            # Confirm that the prepared query is not available anymore on
+            # client 1
+            with pytest.raises(
+                psycopg.OperationalError, match="prepared statement did not exist"
+            ):
+                cur1.execute(prepared_query)
 
 
 def test_parse_larger_than_pkt_buf(bouncer):

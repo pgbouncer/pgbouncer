77a05c1c52d658d5612094dacf3dd6b51fe02576
Jelte Fennema-Nio <jelte.fennema@microsoft.com>
2024-01-11 17:44:28 +0100

    Rename PKT_STARTUP to PKT_STARTUP_V3

--- include/pktbuf.h
+++ include/pktbuf.h
@@ -98,7 +98,7 @@ void pktbuf_write_ExtQuery(PktBuf *buf, const char *query, int nargs, ...);
 	pktbuf_write_generic(buf, PKT_CANCEL, "b", key, 8)
 
 #define pktbuf_write_StartupMessage(buf, user, parms, parms_len) \
-	pktbuf_write_generic(buf, PKT_STARTUP, "bsss", parms, parms_len, "user", user, "")
+	pktbuf_write_generic(buf, PKT_STARTUP_V3, "bsss", parms, parms_len, "user", user, "")
 
 #define pktbuf_write_PasswordMessage(buf, psw) \
 	pktbuf_write_generic(buf, 'p', "s", psw)

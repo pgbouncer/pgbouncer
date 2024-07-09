904fe5826570cfa0ff02babc86a3aa4a028f0411
Dmitry Kovalenko <dmitry.lipetsk@gmail.com>
2024-07-10 00:44:57 +0300

    QA test for issue #1103 [in test_auth.py]

--- test/test_auth.py
+++ test/test_auth.py
@@ -890,3 +890,40 @@ def test_peer_auth_ident_map(bouncer):
         host=f"{bouncer.admin_host}",
         user="bouncer",
     )
+
+
+def test_qa_gh1103__put_in_order(bouncer):
+    """
+    Check that the pgbouncer handles correctly multiple credentials with one name (isue #1103).
+    """
+
+    config = f"""
+        [databases]
+        * = host={bouncer.pg.host} port={bouncer.pg.port} user=postgres min_pool_size=2
+        [pgbouncer]
+        listen_addr = {bouncer.host}
+        listen_port = {bouncer.port}
+
+        auth_type = md5
+        auth_file = {bouncer.auth_path}
+        auth_query = SELECT usename, passwd FROM pg_shadow where usename = $1
+        auth_user = postgres
+        auth_dbname = postgres
+        admin_users = pswcheck
+        logfile = {bouncer.log_path}
+    """
+
+    with bouncer.run_with_config(config):
+        # Let's get an error "no such user"
+        print("POINT #001")
+        with pytest.raises(psycopg.OperationalError, match="no such user"):
+            bouncer.conn(dbname="dummydb2", user="dummyuser2", password="dummypswd2")
+        # Let's wait a few seconds to allow pgbouncer to crash in put_in_order
+        time.sleep(5)
+        # Now we will try to connect with OK parameters
+        print("POINT #002")
+        with bouncer.conn(dbname="p3", user="postgres", password="asdasd") as cn:
+            with cn.cursor() as cur:
+                cur.execute("select 1")
+
+    print("OK!")

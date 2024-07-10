b5d1445826b7c230d12dab500859044f1da7afa0
Dmitry Kovalenko <dmitry.lipetsk@gmail.com>
2024-07-10 22:00:06 +0300

    [fix] test_qa_gh001104 has a problem on Windows

--- test/qa/github/test_qa_gh001104.py
+++ test/qa/github/test_qa_gh001104.py
@@ -26,12 +26,11 @@ def test_qa_gh001104(bouncer):
             listen_addr = {bouncer.host}
             listen_port = {bouncer.port}
 
-            auth_type = md5
+            auth_type = trust
             auth_file = {bouncer.auth_path}
-            auth_query = SELECT usename, passwd FROM pg_shadow where usename = $1
-            auth_user = postgres
-            auth_dbname = postgres
-            admin_users = pswcheck
+
+            admin_users = pgbouncer
+
             logfile = {bouncer.log_path}
         """
 
@@ -40,6 +39,7 @@ def test_qa_gh001104(bouncer):
 
     n = 0
 
-    while n < 50:
+    while n < 10000:
         n = n + 1
+        print("ATTEMPT #",n);
         do_attempt(bouncer, n)

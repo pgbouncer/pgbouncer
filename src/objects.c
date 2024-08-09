80de3398569482180db8f63df25c5b3dee50dbd6
CommanderKeynes <andrewjackson947@gmail.coma>
2024-08-09 05:54:09 -0500

    Format

--- src/objects.c
+++ src/objects.c
@@ -1401,9 +1401,9 @@ void disconnect_server(PgSocket *server, bool send_term, const char *reason, ...
  */
 void disconnect_client(PgSocket *client, bool notify, const char *reason, ...)
 {
-    if (client->login_user_credentials){
-        client->login_user_credentials->global_user->client_connection_count--;
-    }
+	if (client->login_user_credentials) {
+		client->login_user_credentials->global_user->client_connection_count--;
+	}
 	if (reason) {
 		char buf[128];
 		va_list ap;
@@ -2013,8 +2013,8 @@ bool finish_client_login(PgSocket *client)
 	if (!welcome_client(client))
 		return false;
 
-    // Increment client connection count
-    client->login_user_credentials->global_user->client_connection_count++;
+	// Increment client connection count
+	client->login_user_credentials->global_user->client_connection_count++;
 
 	slog_debug(client, "logged in");
 

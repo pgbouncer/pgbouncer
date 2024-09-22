1dbde965e6782f6800eb4d1bb6b4d4002bfd1323
Andrew Jackson <46945903+AndrewJackson2020@users.noreply.github.com>
2024-09-22 09:16:06 -0500

    Add kill_client command (#1147)
    This adds a kill_client command to the admin console. This command is
    the equivalent of the Postgres pg_terminate_backend() function. Similar
    functionality also exists in other Postgres connection poolers.
    
    In general this functionality is a useful last resort to undisciplined
    users opening a lot of connections and which block out other users. This
    is especially prominent with front end GUI DB applications that open a
    new session for every SQL editor tab.

--- test/test_admin.py
+++ test/test_admin.py
@@ -1,3 +1,5 @@
+import psycopg
+import pytest
 from psycopg.rows import dict_row
 
 from .utils import capture, run
@@ -33,6 +35,75 @@ def test_show(bouncer):
 
     for item in show_items:
         bouncer.admin(f"SHOW {item}")
+
+
+def test_kill_client_nonexisting(bouncer):
+    # Connect to client as user A
+    conn_1 = bouncer.conn(dbname="p0", user="maxedout")
+
+    # Validate count
+    clients = bouncer.admin("SHOW CLIENTS")
+    assert len(clients) == 2
+
+    # Get clients id
+    client_id = [client for client in clients if client[2] == "p0"][0][-6]
+    fake_client_id = hex(int(client_id, 16) + 1)
+
+    # Issue kill client command
+    with pytest.raises(
+        psycopg.errors.ProtocolViolation,
+        match=r"client not found",
+    ):
+        clients = bouncer.admin(f"KILL_CLIENT {fake_client_id}")
+
+    # Validate count
+    clients = bouncer.admin("SHOW CLIENTS")
+    assert len(clients) == 2
+
+    conn_1.close()
+
+
+def test_kill_client_invalid(bouncer):
+    # Connect to client as user A
+    conn_1 = bouncer.conn(dbname="p0", user="maxedout")
+
+    # Validate count
+    clients = bouncer.admin("SHOW CLIENTS")
+    assert len(clients) == 2
+
+    # Issue kill client command
+    with pytest.raises(
+        psycopg.errors.ProtocolViolation,
+        match=r"invalid client pointer supplied",
+    ):
+        clients = bouncer.admin("KILL_CLIENT non_existant_client_id")
+
+    # Validate count
+    clients = bouncer.admin("SHOW CLIENTS")
+    assert len(clients) == 2
+
+    conn_1.close()
+
+
+def test_kill_client(bouncer):
+    # Connect to client as user A
+    conn_1 = bouncer.conn(dbname="p0", user="maxedout")
+
+    # Validate count
+    clients = bouncer.admin("SHOW CLIENTS")
+    assert len(clients) == 2
+
+    # Get clients id
+    client_id = [client for client in clients if client[2] == "p0"][0][-6]
+
+    # Issue kill client command
+    clients = bouncer.admin(f"KILL_CLIENT {client_id}")
+
+    # Validate count
+    clients = bouncer.admin("SHOW CLIENTS")
+    assert len(clients) == 1
+
+    conn_1.close()
 
 
 def test_show_version(bouncer):

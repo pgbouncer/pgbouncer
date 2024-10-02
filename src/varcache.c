ac7a617803828998e5d5b52154f63252265d62e8
Brian Cosgrove <cosgroveb@gmail.com>
2024-10-02 02:10:07 +0000

    fixup

--- src/varcache.c
+++ src/varcache.c
@@ -88,7 +88,7 @@ static void init_var_lookup_from_config(const char *cf_track_extra_parameters, i
 
 void init_var_lookup(const char *cf_track_extra_parameters)
 {
-	const char *names[] = { "DateStyle", "client_encoding", "TimeZone", "standard_conforming_strings", "application_name", "in_hot_standby", NULL };
+	const char *names[] = { "DateStyle", "client_encoding", "TimeZone", "standard_conforming_strings" "in_hot_standby", "default_transaction_read_only", NULL };
 	int idx = 0;
 
 	struct var_lookup *lookup = NULL;

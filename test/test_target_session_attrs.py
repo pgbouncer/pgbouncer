f86a3d7aa2d768df6f714ecd52f7083695b50b52
Brian Cosgrove <cosgroveb@gmail.com>
2024-12-13 20:58:03 +0000

    Add test coverage for read-write, read-only, and any target_session_attrs

--- test/test_target_session_attrs.py
+++ test/test_target_session_attrs.py
@@ -48,3 +48,39 @@ def test_target_session_attrs_standby_second(bouncer, replica):
         1,
     ):
         bouncer.test(dbname="standby_second")
+
+
+def test_target_session_attrs_readonly_first(bouncer, replica):
+    with bouncer.log_contains(r"127.0.0.2:\d+ new connection to server", 1):
+        bouncer.test(dbname="readonly_first")
+
+
+def test_target_session_attrs_readonly_second(bouncer, replica):
+    with bouncer.log_contains(
+        r"127.0.0.1:\d+ closing because: server does not satisfy target_session_attrs",
+        1,
+    ):
+        bouncer.test(dbname="readonly_second")
+
+
+def test_target_session_attrs_readwrite_first(bouncer, replica):
+    with bouncer.log_contains(r"127.0.0.1:\d+ new connection to server", 1):
+        bouncer.test(dbname="readwrite_first")
+
+
+def test_target_session_attrs_readwrite_second(bouncer, replica):
+    with bouncer.log_contains(
+        r"127.0.0.2:\d+ closing because: server does not satisfy target_session_attrs",
+        1,
+    ):
+        bouncer.test(dbname="readwrite_second")
+
+
+def test_target_session_attrs_any_primary_first(bouncer, replica):
+    with bouncer.log_contains(r"127.0.0.1:\d+ new connection to server", 1):
+        bouncer.test(dbname="any_primary_first")
+
+
+def test_target_session_attrs_any_primary_second(bouncer, replica):
+    with bouncer.log_contains(r"127.0.0.2:\d+ new connection to server", 1):
+        bouncer.test(dbname="any_primary_second")

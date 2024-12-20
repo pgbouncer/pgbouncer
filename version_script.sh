943f446a7ed9da4eb4ec607c1faefbd37288dcd4
Andrew Jackson <andrewjackson2988@gmail.com>
2024-12-20 08:58:36 -0600

    fix script

--- version_script.sh
+++ version_script.sh
@@ -1,1 +1,7 @@
-echo 1.23.1-$(git describe --always --abbrev=5 --exclude "*" || echo "unknown" )
+git_hash=$(git describe --always --abbrev=5 --exclude "*" || echo unknown)
+numeric_version="1.23.1"
+if [[ $git_hash == "unknown" ]]; then
+  printf $numeric_version
+else
+  printf $numeric_version-$git_hash
+fi

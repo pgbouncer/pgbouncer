63fc942a335460e27674118f2e43a06099cca8cd
CommanderKeynes <andrewjackson947@gmail.coma>
2025-05-16 21:45:46 -0500

    Fix another case

--- test/utils.py
+++ test/utils.py
@@ -376,7 +376,7 @@ class QueryRunner:
             try:
                 return await cur.fetchall()
             except psycopg.ProgrammingError as e:
-                if "the last operation didn't produce a result" == str(e):
+                if "the last operation didn't produce records" in str(e):
                     return None
                 raise
 

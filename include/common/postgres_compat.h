dab1a4cbc5c7bc51397da13823a44a0754aa5077
Jelte Fennema-Nio <postgres@jeltef.nl>
2025-07-14 22:59:17 +0200

    Fix compilation with asserts

--- include/common/postgres_compat.h
+++ include/common/postgres_compat.h
@@ -10,11 +10,17 @@
 #include <usual/base.h>
 #include <usual/ctype.h>
 
+
+#ifdef CASSERT
+#define USE_ASSERT_CHECKING
+#endif
+
 #define int8 int8_t
 #define uint8 uint8_t
 #define uint16 uint16_t
 #define uint32 uint32_t
 #define uint64 uint64_t
+#define Size size_t
 
 #define lengthof(array) (sizeof (array) / sizeof ((array)[0]))
 #define pg_hton32(x) htobe32(x)

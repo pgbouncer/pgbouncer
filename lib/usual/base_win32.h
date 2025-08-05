0595e105d3022b5ed6bee3ee0ce9d7eb7810fbb3
Peter Eisentraut <peter@eisentraut.org>
2025-08-05 20:17:28 +0200

    usual: Remove check for inttypes.h
    This has been standard since C99.

--- lib/usual/base_win32.h
+++ lib/usual/base_win32.h
@@ -137,39 +137,4 @@ static inline struct group *getgrgid(gid_t gid)
 	return NULL;
 }
 
-/* format specifiers that should be in <inttypes.h> */
-#ifndef HAVE_INTTYPES_H
-
-#define PRId8   "d"
-#define PRId16  "d"
-#define PRId32  "d"
-#define PRId64  "I64d"
-
-#define PRIi8   "d"
-#define PRIi16  "d"
-#define PRIi32  "d"
-#define PRIi64  "I64d"
-
-#define PRIo8   "o"
-#define PRIo16  "o"
-#define PRIo32  "o"
-#define PRIo64  "I64o"
-
-#define PRIu8   "u"
-#define PRIu16  "u"
-#define PRIu32  "u"
-#define PRIu64  "I64u"
-
-#define PRIx8   "x"
-#define PRIx16  "x"
-#define PRIx32  "x"
-#define PRIx64  "I64x"
-
-#define PRIX8   "X"
-#define PRIX16  "X"
-#define PRIX32  "X"
-#define PRIX64  "I64X"
-
-#endif
-
 #endif

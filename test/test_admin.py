efa64d0c0693fa657282b8e0dcd17f94374c872c
Euler Taveira <euler@eulerto.com>
2026-08-06 17:44:51 -0300

    C405: Unnecessary list literal (rewrite as a set literal)

--- test/test_admin.py
+++ test/test_admin.py
@@ -139,13 +139,11 @@ def test_socket_id(bouncer) -> None:
                 time.sleep(2)
                 clients = admin_cursor.execute("SHOW SOCKETS").fetchall()
                 assert len(clients) == 3
-                assert set(
-                    [
+                assert {
                         initial_id,
                         initial_id + i * 2 - 1,
                         initial_id + i * 2,
-                    ]
-                ) == {client["id"] for client in clients}
+                    } == {client["id"] for client in clients}
                 conn_2.close()
                 time.sleep(2)
 

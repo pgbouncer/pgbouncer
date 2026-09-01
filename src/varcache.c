3227e9b216d96178e9c6a48ff04255f5d5ab1359
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2026-09-01 13:58:44 +0300

    Track default_transaction_read_only in the varcache
    Before this commit, if a client did `SET default_transaction_read_only
    = true` in transaction mode, the connection was returned to the pool
    in read only mode, causing subsequent write transaction to fail. This
    is a well-known problem, and the common work around is to add
    `default_transaction_read_only` to `track_extra_parameters`. However,
    we can easily fix it for everyone by adding it to the static list of
    variables that are always tracked.
    
    `default_transaction_read_only` has been marked as `GUC_REPORT` since
    PostgreSQL 14. In older server versions, we still cannot track it, but
    that's OK.

--- src/varcache.c
+++ src/varcache.c
@@ -88,7 +88,15 @@ static void init_var_lookup_from_config(const char *cf_track_extra_parameters, i
 
 void init_var_lookup(const char *cf_track_extra_parameters)
 {
-	const char *names[] = { "DateStyle", "client_encoding", "TimeZone", "standard_conforming_strings", "application_name", NULL };
+	const char *names[] = {
+		"DateStyle",
+		"client_encoding",
+		"TimeZone",
+		"standard_conforming_strings",
+		"application_name",
+		"default_transaction_read_only",
+		NULL
+	};
 	int idx = 0;
 
 	struct var_lookup *lookup = NULL;

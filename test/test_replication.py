pgbouncer/pgbouncer

commit e1c6c2f2269539ae05c3f2a6fb03506340d90102

Support replication connections through PgBouncer (#876)
In session pooling mode PgBouncer is pretty much a transparent proxy,
i.e. the client does normally not even need to know that PgBouncer is in
the middle. This allows things like load balancing and failovers without
the client needing to know about this at all. But as soon as replication
connections are needed, this was not possible anymore, because PgBouncer
would reject those instead of proxying them to the right server.

This PR fixes that by also proxying replication connections. They are
handled pretty differently from normal connections though. A client and
server replication connection will form a strong pair, as soon as one is
closed the other is closed too. So, there's no caching of the server
replication connections, like is done for regular connections. Reusing
replication connections comes with a ton of gotchas. Postgres will
throw errors in many cases when trying to do so. So simply not
doing it seems like a good tradeoff for ease of implementation.
Especially because replication connections are pretty much always
very long lived. So re-using them gains pretty much no performance
benefits.

Fixes #382

Depends on #945

src/objects.c
@@ -83,6 +83,12 @@ static STATLIST(justfree_server_list);
 /* init autodb idle list */
 STATLIST(autodatabase_idle_list);
 
+const char *replication_type_parameters[] = {
+	[REPLICATION_NONE] = "no",
+	[REPLICATION_LOGICAL] = "database",
+	[REPLICATION_PHYSICAL] = "yes",
+};
+
 /* fast way to get number of active clients */
 int get_active_client_count(void)
 {
@@ -777,10 +783,12 @@ PgPool *get_peer_pool(PgDatabase *db)
 /* deactivate socket and put into wait queue */
 static void pause_client(PgSocket *client)
 {
+	SocketState newstate;
 	Assert(client->state == CL_ACTIVE || client->state == CL_LOGIN);
-
 	slog_debug(client, "pause_client");
-	change_client_state(client, CL_WAITING);
+	newstate = CL_WAITING;
+
+	change_client_state(client, newstate);
 	if (!sbuf_pause(&client->sbuf))
 		disconnect_client(client, true, "pause failed");
 }
@@ -888,6 +896,17 @@ bool find_server(PgSocket *client)
 	/* try to get idle server, if allowed */
 	if (cf_pause_mode == P_PAUSE || pool->db->db_paused) {
 		server = NULL;
+	} else if (client->replication) {
+		/*
+		 * For replication clients we open dedicated server connections. These
+		 * connections are linked to a client as soon as the server is ready,
+		 * instead of lazily being assigned to a client only when the client
+		 * sends a query. So if we reach this point we know that that has not
+		 * happened yet, and we need to create a new replication connection for
+		 * this client.
+		 */
+		launch_new_connection(pool, /*evict_if_needed= */ true);
+		server = NULL;
 	} else {
 		while (1) {
 			server = first_socket(&pool->idle_server_list);
@@ -944,8 +963,11 @@ static bool reuse_on_release(PgSocket *server)
 {
 	bool res = true;
 	PgPool *pool = server->pool;
-	PgSocket *client = first_socket(&pool->waiting_client_list);
-	if (client) {
+	PgSocket *client;
+	Assert(!server->replication);
+	slog_debug(server, "reuse_on_release: replication %d", server->replication);
+	client = first_socket(&pool->waiting_client_list);
+	if (client && !client->replication) {
 		activate_client(client);
 
 		/*
@@ -1151,7 +1173,7 @@ bool release_server(PgSocket *server)
 		}
 
 		if (*cf_server_reset_query && (cf_server_reset_query_always ||
-					       pool_pool_mode(pool) == POOL_SESSION)) {
+					       connection_pool_mode(server) == POOL_SESSION)) {
 			/* notify reset is required */
 			newstate = SV_TESTED;
 		} else if (cf_server_check_delay == 0 && *cf_server_check_query) {
@@ -1217,6 +1239,18 @@ bool release_server(PgSocket *server)
 		return true;
 	}
 
+	if (server->replication) {
+		if (server->link) {
+			slog_debug(server, "release_server: new replication connection ready");
+			change_server_state(server, SV_ACTIVE);
+			activate_client(server->link);
+			return true;
+		} else {
+			disconnect_server(server, true, "replication client was closed");
+			return false;
+		}
+	}
+
 	Assert(server->link == NULL);
 	slog_noise(server, "release_server: new state=%d", newstate);
 	change_server_state(server, newstate);
@@ -1229,6 +1263,28 @@ bool release_server(PgSocket *server)
 	}
 
 	return true;
+}
+
+static void unlink_server(PgSocket *server, const char *reason)
+{
+	PgSocket *client;
+	if (!server->link)
+		return;
+
+	client = server->link;
+
+	client->link = NULL;
+	server->link = NULL;
+	/*
+	 * Send reason to client if it is already
+	 * logged in, otherwise send generic message.
+	 */
+	if (client->state == CL_ACTIVE || client->state == CL_WAITING)
+		disconnect_client(client, true, "%s", reason);
+	else if (client->state == CL_ACTIVE_CANCEL)
+		disconnect_client(client, false, "successfully sent cancel request");
+	else
+		disconnect_client(client, true, "bouncer config error");
 }
 
 /*
@@ -1262,26 +1318,9 @@ void disconnect_server(PgSocket *server, bool send_term, const char *reason, ...
 
 	switch (server->state) {
 	case SV_ACTIVE_CANCEL:
-	case SV_ACTIVE: {
-		PgSocket *client = server->link;
-
-		if (client) {
-			client->link = NULL;
-			server->link = NULL;
-			/*
-			 * Send reason to client if it is already
-			 * logged in, otherwise send generic message.
-			 */
-			if (client->state == CL_ACTIVE || client->state == CL_WAITING)
-				disconnect_client(client, true, "%s", reason);
-			else if (client->state == CL_ACTIVE_CANCEL)
-				disconnect_client(client, false, "successfully sent cancel request");
-			else
-				disconnect_client(client, true, "bouncer config error");
-		}
-
+	case SV_ACTIVE:
+		unlink_server(server, reason);
 		break;
-	}
 	case SV_TESTED:
 	case SV_USED:
 	case SV_IDLE:
@@ -1305,6 +1344,8 @@ void disconnect_server(PgSocket *server, bool send_term, const char *reason, ...
 			server->pool->last_connect_failed = false;
 			send_term = false;
 		}
+		if (server->replication)
+			unlink_server(server, reason);
 		break;
 	default:
 		fatal("bad server state: %d", server->state);
@@ -1466,6 +1507,16 @@ void disconnect_client_sqlstate(PgSocket *client, bool notify, const char *sqlst
 		break;
 	case CL_WAITING:
 	case CL_WAITING_LOGIN:
+		/*
+		 * replication connections might already be linked to a server
+		 * while they are still in a waiting state.
+		 */
+		if (client->replication && client->link) {
+			PgSocket *server = client->link;
+			server->link = NULL;
+			client->link = NULL;
+			disconnect_server(server, false, "replication client disconnected");
+		}
 		break;
 	default:
 		fatal("bad client state: %d", client->state);
@@ -1490,6 +1541,9 @@ void disconnect_client_sqlstate(PgSocket *client, bool notify, const char *sqlst
 		free(client->db);
 		client->db = NULL;
 	}
+
+	free(client->startup_options);
+	client->startup_options = NULL;
 
 	change_client_state(client, CL_JUSTFREE);
 	if (!sbuf_close(&client->sbuf))
@@ -1685,6 +1739,21 @@ bool evict_connection(PgDatabase *db)
 	return false;
 }
 
+/* evict the oldest idle connection from the pool */
+bool evict_pool_connection(PgPool *pool)
+{
+	PgSocket *oldest_connection = NULL;
+
+	oldest_connection = compare_connections_by_time(oldest_connection, last_socket(&pool->idle_server_list));
+
+	if (oldest_connection) {
+		disconnect_server(oldest_connection, true, "evicted");
+		return true;
+	}
+	return false;
+}
+
+
 /* evict the single most idle connection from among all pools to make room in the user */
 bool evict_user_connection(PgCredentials *user_credentials)
 {
@@ -1725,6 +1794,7 @@ void launch_new_connection(PgPool *pool, bool evict_if_needed)
 	PgSocket *server;
 	int max;
 
+	log_debug("launch_new_connection: start");
 	/*
 	 * Allow only a single connection attempt at a time.
 	 *
@@ -1780,15 +1850,24 @@ void launch_new_connection(PgPool *pool, bool evict_if_needed)
 	/* is it allowed to add servers? */
 	if (max >= pool_pool_size(pool) && pool->welcome_msg_ready) {
 		/* should we use reserve pool? */
+		PgSocket *c = first_socket(&pool->waiting_client_list);
 		if (cf_res_pool_timeout && pool_res_pool_size(pool)) {
 			usec_t now = get_cached_time();
-			PgSocket *c = first_socket(&pool->waiting_client_list);
 			if (c && (now - c->request_time) >= cf_res_pool_timeout) {
 				if (max < pool_pool_size(pool) + pool_res_pool_size(pool)) {
 					slog_warning(c, "taking connection from reserve_pool");
 					goto allow_new;
 				}
 			}
+		}
+
+		if (c && c->replication) {
+			while (evict_if_needed && pool_pool_size(pool) >= max) {
+				if (!evict_pool_connection(pool))
+					break;
+			}
+			if (pool_pool_size(pool) < max)
+				goto allow_new;
 		}
 		log_debug("launch_new_connection: pool full (%d >= %d)",
 			  max, pool_pool_size(pool));

test/test_limits.py
@@ -98,7 +98,7 @@ def test_min_pool_size_with_lower_max_user_connections(bouncer):
 
     # Running a query for sufficient time for us to reach the final
     # connection count in the pool and detect any evictions.
-    with bouncer.log_contains("new connection to server", times=2):
+    with bouncer.log_contains(r"new connection to server \(from", times=2):
         with bouncer.log_contains("closing because: evicted", times=0):
             bouncer.sleep(2, dbname="p0x", user="maxedout2")
 
@@ -111,7 +111,7 @@ def test_min_pool_size_with_lower_max_db_connections(bouncer):
 
     # Running a query for sufficient time for us to reach the final
     # connection count in the pool and detect any evictions.
-    with bouncer.log_contains("new connection to server", times=2):
+    with bouncer.log_contains(r"new connection to server \(from", times=2):
         with bouncer.log_contains("closing because: evicted", times=0):
             bouncer.sleep(2, dbname="p0y", user="puser1")
 

test/test_replication.py
@@ -1,0 +1,281 @@
+import asyncio
+import signal
+import subprocess
+import time
+
+import psycopg
+import psycopg.errors
+import pytest
+from psycopg import sql
+
+from .utils import PG_MAJOR_VERSION, WINDOWS, run
+
+
+def test_logical_rep(bouncer):
+    connect_args = {
+        "dbname": "user_passthrough",
+        "replication": "database",
+        "user": "postgres",
+        "application_name": "abc",
+        "options": "-c enable_seqscan=off",
+    }
+    # Starting in PG10 you can do other commands over logical rep connections
+    if PG_MAJOR_VERSION >= 10:
+        bouncer.test(**connect_args)
+        assert bouncer.sql_value("SHOW application_name", **connect_args) == "abc"
+        assert bouncer.sql_value("SHOW enable_seqscan", **connect_args) == "off"
+    bouncer.sql("IDENTIFY_SYSTEM", **connect_args)
+    # Do a normal connection to the same pool, to ensure that that doesn't
+    # break anything
+    bouncer.test(dbname="user_passthrough", user="postgres")
+    bouncer.sql("IDENTIFY_SYSTEM", **connect_args)
+
+
+def test_logical_rep_unprivileged(bouncer):
+    if PG_MAJOR_VERSION < 10:
+        expected_log = "no pg_hba.conf entry for replication connection"
+    elif PG_MAJOR_VERSION < 16:
+        expected_log = "must be superuser or replication role to start walsender"
+    else:
+        expected_log = "permission denied to start WAL sender"
+
+    with bouncer.log_contains(
+        expected_log,
+    ), bouncer.log_contains(
+        r"closing because: login failed \(age", times=2
+    ), pytest.raises(psycopg.OperationalError, match=r"login failed"):
+        bouncer.sql("IDENTIFY_SYSTEM", replication="database")
+
+
+@pytest.mark.skipif(
+    "PG_MAJOR_VERSION < 10", reason="logical replication was introduced in PG10"
+)
+def test_logical_rep_subscriber(bouncer):
+    bouncer.admin("set pool_mode=transaction")
+
+    # First write create a table and insert a row in the source database.
+    # Also create the replication slot and publication
+    bouncer.default_db = "user_passthrough"
+    bouncer.create_schema("test_logical_rep_subscriber")
+    bouncer.sql("CREATE TABLE test_logical_rep_subscriber.table(a int)")
+    bouncer.sql("INSERT INTO test_logical_rep_subscriber.table values (1)")
+    assert (
+        bouncer.sql_value("SELECT count(*) FROM test_logical_rep_subscriber.table") == 1
+    )
+
+    bouncer.create_publication(
+        "mypub", sql.SQL("FOR TABLE test_logical_rep_subscriber.table")
+    )
+
+    bouncer.create_logical_replication_slot("test_logical_rep_subscriber", "pgoutput")
+
+    # Create an equivalent, but empty schema in the target database.
+    # And setup the subscription
+    bouncer.default_db = "user_passthrough2"
+    bouncer.create_schema("test_logical_rep_subscriber")
+    bouncer.sql("CREATE TABLE test_logical_rep_subscriber.table(a int)")
+    conninfo = bouncer.make_conninfo(dbname="user_passthrough")
+    bouncer.create_subscription(
+        "mysub",
+        sql.SQL(
+            """
+            CONNECTION {}
+            PUBLICATION mypub
+            WITH (slot_name=test_logical_rep_subscriber, create_slot=false)
+        """
+        ).format(sql.Literal(conninfo)),
+    )
+
+    # The initial copy should now copy over the row
+    time.sleep(2)
+    assert (
+        bouncer.sql_value("SELECT count(*) FROM test_logical_rep_subscriber.table") >= 1
+    )
+
+    # Insert another row and logical replication should replicate it correctly
+    bouncer.sql(
+        "INSERT INTO test_logical_rep_subscriber.table values (2)",
+        dbname="user_passthrough",
+    )
+    time.sleep(2)
+    assert (
+        bouncer.sql_value("SELECT count(*) FROM test_logical_rep_subscriber.table") >= 2
+    )
+
+
+@pytest.mark.skipif(
+    "WINDOWS", reason="MINGW does not have contrib package containing test_decoding"
+)
+def test_logical_rep_pg_recvlogical(bouncer):
+    bouncer.default_db = "user_passthrough"
+    bouncer.create_schema("test_logical_rep_pg_recvlogical")
+    bouncer.sql("CREATE TABLE test_logical_rep_pg_recvlogical.table(a int)")
+    bouncer.create_logical_replication_slot(
+        "test_logical_rep_pg_recvlogical", "test_decoding"
+    )
+    process = subprocess.Popen(
+        [
+            "pg_recvlogical",
+            "--dbname",
+            bouncer.default_db,
+            "--host",
+            bouncer.host,
+            "--port",
+            str(bouncer.port),
+            "--user",
+            bouncer.default_user,
+            "--slot=test_logical_rep_pg_recvlogical",
+            "--file=-",
+            "--no-loop",
+            "--start",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    assert process.stdout is not None
+    bouncer.sql("INSERT INTO test_logical_rep_pg_recvlogical.table values (1)")
+    try:
+        assert process.stdout.readline().startswith(b"BEGIN ")
+        assert (
+            process.stdout.readline()
+            == b'table test_logical_rep_pg_recvlogical."table": INSERT: a[integer]:1\n'
+        )
+        assert process.stdout.readline().startswith(b"COMMIT ")
+    finally:
+        process.kill()
+        process.communicate(timeout=5)
+
+
+def test_physical_rep(bouncer):
+    connect_args = {
+        "dbname": "user_passthrough",
+        "replication": "yes",
+        "user": "postgres",
+        "application_name": "abc",
+        "options": "-c enable_seqscan=off",
+    }
+    # Starting in PG10 you can do SHOW commands
+    if PG_MAJOR_VERSION >= 10:
+        with pytest.raises(
+            psycopg.errors.FeatureNotSupported,
+            match="cannot execute SQL commands in WAL sender for physical replication",
+        ):
+            bouncer.test(**connect_args)
+        assert bouncer.sql_value("SHOW application_name", **connect_args) == "abc"
+        assert bouncer.sql_value("SHOW enable_seqscan", **connect_args) == "off"
+    bouncer.sql("IDENTIFY_SYSTEM", **connect_args)
+    # Do a normal connection to the same pool, to ensure that that doesn't
+    # break anything
+    bouncer.test(dbname="user_passthrough", user="postgres")
+    bouncer.sql("IDENTIFY_SYSTEM", **connect_args)
+
+
+def test_physcal_rep_unprivileged(bouncer):
+    with bouncer.log_contains(
+        r"no pg_hba.conf entry for replication connection from host"
+    ), bouncer.log_contains(
+        r"closing because: login failed \(age", times=2
+    ), pytest.raises(
+        psycopg.OperationalError, match=r"login failed"
+    ):
+        bouncer.test(replication="yes")
+
+
+@pytest.mark.skipif("PG_MAJOR_VERSION < 10", reason="pg_receivewal was added in PG10")
+def test_physical_rep_pg_receivewal(bouncer, tmp_path):
+    bouncer.default_db = "user_passthrough"
+    bouncer.create_physical_replication_slot("test_physical_rep_pg_receivewal")
+    wal_dump_dir = tmp_path / "wal-dump"
+    wal_dump_dir.mkdir()
+
+    process = subprocess.Popen(
+        [
+            "pg_receivewal",
+            "--dbname",
+            bouncer.make_conninfo(),
+            "--slot=test_physical_rep_pg_receivewal",
+            "--directory",
+            str(wal_dump_dir),
+        ],
+    )
+    time.sleep(3)
+
+    if WINDOWS:
+        process.terminate()
+    else:
+        process.send_signal(signal.SIGINT)
+    process.communicate(timeout=5)
+
+    if WINDOWS:
+        assert process.returncode == 1
+    else:
+        assert process.returncode == 0
+
+    children = list(wal_dump_dir.iterdir())
+    assert len(children) > 0
+
+
+def test_physical_rep_pg_basebackup(bouncer, tmp_path):
+    bouncer.default_db = "user_passthrough"
+    dump_dir = tmp_path / "db-dump"
+    dump_dir.mkdir()
+
+    run(
+        [
+            "pg_basebackup",
+            "--dbname",
+            bouncer.make_conninfo(),
+            "--checkpoint=fast",
+            "--pgdata",
+            str(dump_dir),
+        ],
+        shell=False,
+    )
+    children = list(dump_dir.iterdir())
+    assert len(children) > 0
+    print(children)
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    "PG_MAJOR_VERSION < 10",
+    reason="normal SQL commands are only supported in PG10+ on logical replication connections",
+)
+async def test_replication_pool_size(pg, bouncer):
+    connect_args = {
+        "dbname": "user_passthrough_pool_size2",
+        "replication": "database",
+        "user": "postgres",
+        "connect_timeout": 10,
+    }
+    start = time.time()
+    await bouncer.asleep(0.5, times=10, **connect_args)
+    assert time.time() - start > 2.5
+    # Replication connections always get closed right away
+    assert pg.connection_count("p0") == 0
+
+    connect_args["dbname"] = "user_passthrough_pool_size5"
+    start = time.time()
+    await bouncer.asleep(0.5, times=10, **connect_args)
+    assert time.time() - start > 1
+    # Replication connections always get closed right away
+    assert pg.connection_count("p0") == 0
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    "PG_MAJOR_VERSION < 10",
+    reason="normal SQL commands are only supported in PG10+ on logical replication connections",
+)
+async def test_replication_pool_size_mixed_clients(bouncer):
+    connect_args = {
+        "dbname": "user_passthrough_pool_size2",
+        "user": "postgres",
+    }
+
+    # Fill the pool with normal connections
+    await bouncer.asleep(0.5, times=2, **connect_args)
+
+    # Then try to open a replication connection and ensure that it causes
+    # eviction of one of the normal connections
+    with bouncer.log_contains("closing because: evicted"):
+        bouncer.test(**connect_args, replication="database")

commit 679aa7652d4c203c4b0f1368998b54e55954637e

Fix eviction for replication clients and log which limit caused it
The launch_new_connection() lets a replication client evict idle servers
once its pool is already at pool_size, but the loop that does the
evicting compared the count and the limit backwards:
`pool_pool_size(pool) >= max`, against a `max` snapshot that nothing
updated as evictions happened.

That went wrong in two ways: (a) Once the pool was exactly full, the
condition stayed true until evict_pool_connection() ran out of idle
servers, so a single replication client closed every idle server in the
pool instead of just enough to make room and (b) a pool holding more
servers than pool_size, which is possible through the reserve pool or
the cancel-request path that allows up to twice pool_size, never entered
the loop. Nothing was evicted and the client just opened another
connection on top.

Compare the count against the limit the right way round, and re-read the
server count after each eviction so the loop stops as soon as there is
room. Re-reading also keeps the "pool full (%d >= %d)" debug line
honest, since it now reports the count after evictions rather than
before. Since the value is now a live server count rather than a limit,
call it server_count instead of max in that part of the function.

Separately, evict_connection(), evict_pool_connection() and
evict_user_connection() each close a server to make room under a
different limit, but all three logged the same bare "evicted" reason.
Every other disconnect reason says what happened, so this was the one
case where the log could not tell which limit was full. Since each
function serves exactly one limit, give each its own reason ("evicted
for max_db_connections", "evicted for pool_size" and "evicted for
max_user_connections"). The "evicted" prefix is kept, so a log filter
for that word still matches every eviction.

For the tests, test_replication_pool_size_mixed_clients now asserts
exactly one eviction, on the pool_size reason, instead of just checking
that some eviction happened. The
test_replication_pool_size_over_full_pool covers the over-full pool by
putting a server in the reserve pool, which the janitor leaves alone.
Finally, test_max_db_connections and the new
test_max_user_connections_eviction check for the reason of the limit they
exercise. The patterns are anchored on " (age=" so that a longer reason
with the same prefix cannot satisfy them. The user limit gets its own
test because test_max_user_connections does not always evict: its three
pools can each get a server without touching the others, so whether an
eviction happens depends on timing.

The bug was introduced by e1c6c2f, which added replication support.

src/objects.c
@@ -1800,7 +1800,7 @@ bool evict_connection(PgDatabase *db)
 	}
 
 	if (oldest_connection) {
-		disconnect_server(oldest_connection, true, "evicted");
+		disconnect_server(oldest_connection, true, "evicted for max_db_connections");
 		return true;
 	}
 	return false;
@@ -1814,7 +1814,7 @@ bool evict_pool_connection(PgPool *pool)
 	oldest_connection = compare_connections_by_time(oldest_connection, last_socket(&pool->idle_server_list));
 
 	if (oldest_connection) {
-		disconnect_server(oldest_connection, true, "evicted");
+		disconnect_server(oldest_connection, true, "evicted for pool_size");
 		return true;
 	}
 	return false;
@@ -1841,7 +1841,7 @@ bool evict_user_connection(PgCredentials *user_credentials)
 	}
 
 	if (oldest_connection) {
-		disconnect_server(oldest_connection, true, "evicted");
+		disconnect_server(oldest_connection, true, "evicted for max_user_connections");
 		return true;
 	}
 	return false;
@@ -1859,6 +1859,7 @@ bool evict_user_connection(PgCredentials *user_credentials)
 void launch_new_connection(PgPool *pool, bool evict_if_needed)
 {
 	PgSocket *server;
+	int server_count;
 	int max;
 
 	log_debug("launch_new_connection: start");
@@ -1886,17 +1887,17 @@ void launch_new_connection(PgPool *pool, bool evict_if_needed)
 		}
 	}
 
-	max = pool_server_count(pool);
+	server_count = pool_server_count(pool);
 
 	/*
 	 * Peer pools only have a single pool_size.
 	 */
 	if (pool->db->peer_id) {
-		if (max < pool_pool_size(pool))
+		if (server_count < pool_pool_size(pool))
 			goto force_new;
 
 		log_debug("launch_new_connection: peer pool full (%d >= %d)",
-			  max, pool_pool_size(pool));
+			  server_count, pool_pool_size(pool));
 		return;
 	}
 
@@ -1909,20 +1910,20 @@ void launch_new_connection(PgPool *pool, bool evict_if_needed)
 	 * this works just fine, because we only ever open a single connection at
 	 * once (see top of this function).
 	 */
-	if (!statlist_empty(&pool->waiting_cancel_req_list) && max < (2 * pool_pool_size(pool))) {
+	if (!statlist_empty(&pool->waiting_cancel_req_list) && server_count < (2 * pool_pool_size(pool))) {
 		log_debug("launch_new_connection: bypass pool limitations for cancel request");
 		goto force_new;
 	}
 
 	/* is it allowed to add servers? */
 	if (pool_pool_size(pool) > 0) {
-		if (max >= pool_pool_size(pool) && pool->welcome_msg_ready) {
+		if (server_count >= pool_pool_size(pool) && pool->welcome_msg_ready) {
 			/* should we use reserve pool? */
 			PgSocket *c = first_socket(&pool->waiting_client_list);
 			if (cf_res_pool_timeout && pool_res_pool_size(pool)) {
 				usec_t now = get_cached_time();
 				if (c && (now - c->request_time) >= cf_res_pool_timeout) {
-					if (max < pool_pool_size(pool) + pool_res_pool_size(pool)) {
+					if (server_count < pool_pool_size(pool) + pool_res_pool_size(pool)) {
 						slog_warning(c, "taking connection from reserve_pool");
 						goto allow_new;
 					}
@@ -1930,15 +1931,16 @@ void launch_new_connection(PgPool *pool, bool evict_if_needed)
 			}
 
 			if (c && c->replication && !sending_auth_query(c)) {
-				while (evict_if_needed && pool_pool_size(pool) >= max) {
+				while (evict_if_needed && server_count >= pool_pool_size(pool)) {
 					if (!evict_pool_connection(pool))
 						break;
+					server_count = pool_server_count(pool);
 				}
-				if (pool_pool_size(pool) < max)
+				if (server_count < pool_pool_size(pool))
 					goto allow_new;
 			}
 			log_debug("launch_new_connection: pool full (%d >= %d)",
-				  max, pool_pool_size(pool));
+				  server_count, pool_pool_size(pool));
 			return;
 		}
 	}

test/test_limits.py
@@ -595,10 +595,13 @@ async def test_max_db_connections(pg, bouncer):
     # some users, doesn't matter which ones
     users = ["muser1", "muser2", "puser1", "puser2", "postgres"]
 
-    # p2 has max_db_connections=4
-    await asyncio.gather(
-        *[bouncer.asleep(0.5, dbname="p2", user=u, times=2) for u in users]
-    )
+    # p2 has max_db_connections=4, so the five pools have to take connections
+    # off each other to get all their clients served, and the log should name
+    # the limit that made them do it.
+    with bouncer.log_contains(r"closing because: evicted for max_db_connections \(age"):
+        await asyncio.gather(
+            *[bouncer.asleep(0.5, dbname="p2", user=u, times=2) for u in users]
+        )
 
     # p2 in PgBouncer maps to p0 in Postgres
     assert pg.connection_count("p0", users=users) == 4
@@ -616,3 +619,24 @@ async def test_max_user_connections(pg, bouncer):
     )
 
     assert pg.connection_count("p7", users=["maxedout"]) == 3
+
+
+async def test_max_user_connections_eviction(pg, bouncer):
+    # maxedout has max_user_connections=3. Three concurrent clients on p7a need
+    # three servers, which uses up the user's whole budget and leaves those
+    # servers idle afterwards. A client on p7b, a separate pool over the same
+    # Postgres database, then has no way in except by evicting one of them,
+    # and one is all it needs.
+    #
+    # This is done in two steps on purpose. With clients on several pools at
+    # once, a pool that gets its server before anyone else needs it never has
+    # to evict, so whether an eviction happens at all depends on timing.
+    await bouncer.asleep(
+        0.5, dbname="p7a", user="maxedout", times=3, connect_timeout=10
+    )
+    assert pg.connection_count("p7", users=["maxedout"]) == 3
+
+    with bouncer.log_contains(
+        r"closing because: evicted for max_user_connections \(age", times=1
+    ):
+        bouncer.test(dbname="p7b", user="maxedout", connect_timeout=10)

test/test_replication.py
@@ -293,6 +293,34 @@ async def test_replication_pool_size_mixed_clients(bouncer):
     await bouncer.asleep(0.5, times=2, **connect_args)
 
     # Then try to open a replication connection and ensure that it causes
-    # eviction of one of the normal connections
-    with bouncer.log_contains("closing because: evicted"):
+    # eviction of one of the normal connections. One, not both: the pool is
+    # exactly full, so the first eviction is all the room the replication
+    # client needs.
+    with bouncer.log_contains(r"closing because: evicted for pool_size \(age", times=1):
+        bouncer.test(**connect_args, replication="database")
+
+
+@pytest.mark.skipif(
+    "PG_MAJOR_VERSION < 10",
+    reason="normal SQL commands are only supported in PG10+ on logical replication connections",
+)
+async def test_replication_pool_size_over_full_pool(bouncer):
+    connect_args = {
+        "dbname": "user_passthrough_pool_size2",
+        "user": "postgres",
+    }
+
+    # Let the pool go over its pool_size by using the reserve pool. The
+    # janitor leaves this alone, since it only closes servers beyond
+    # pool_size + reserve_pool_size.
+    bouncer.write_ini("[users]\npostgres = pool_size=1 reserve_pool_size=1")
+    bouncer.admin("RELOAD")
+    bouncer.admin("SET reserve_pool_timeout = 1")
+    with bouncer.log_contains("taking connection from reserve_pool", times=1):
+        await bouncer.asleep(2, times=2, **connect_args)
+
+    # Both servers are idle now, one more than pool_size allows. A replication
+    # client has to evict enough of them to get below pool_size rather than
+    # open yet another connection on top.
+    with bouncer.log_contains(r"closing because: evicted for pool_size \(age", times=2):
         bouncer.test(**connect_args, replication="database")